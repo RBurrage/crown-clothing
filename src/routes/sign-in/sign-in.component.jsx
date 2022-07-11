70bbb7209119f898704b5f250d80914e30fdef25
Robin Burrage <robin.o.burrage@gmail.com>
2022-07-11 19:07:03 -0400

    Sync with ZTM code

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,20 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import {
+    signInWithGooglePopup,
+    createUserProfileDocument,
+} from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
     const logGoogleUser = async () => {
-        const { user } = await signInWithGooglePopup();
-        createUserDocumentFromAuth(user)
-    }
+        const response = await signInWithGooglePopup();
+        createUserProfileDocument(response);
+    };
+
     return (
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
         </div>
-    )
+    );
 };
 
 export default SignIn;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,63 +1,32 @@
 import { initializeApp } from 'firebase/app';
 import {
     getAuth,
-    signInWithRedirect,
     signInWithPopup,
-    GoogleAuthProvider
+    GoogleAuthProvider,
 } from 'firebase/auth';
 
-import {
-    getFirestore,
-    doc,
-    getDoc,
-    setDoc
-} from 'firebase/firestore';
-
 const firebaseConfig = {
-    apiKey: "AIzaSyC2W8mtaJwicMxAOrULg9SjkKSsVdMfADw",
-    authDomain: "crown-clothing-db-2ddc6.firebaseapp.com",
-    projectId: "crown-clothing-db-2ddc6",
-    storageBucket: "crown-clothing-db-2ddc6.appspot.com",
-    messagingSenderId: "1029331093917",
-    appId: "1:1029331093917:web:fbbcd268a78b077a83c774"
+    apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
+    authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
+    projectId: 'crwn-clothing-db-98d4d',
+    storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
+    messagingSenderId: '626766232035',
+    appId: '1:626766232035:web:506621582dab103a4d08d6',
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = (new GoogleAuthProvider());
+const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-    prompt: "select_account"
+    prompt: 'select_account',
 });
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    console.log(userAuth);
+};
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
-
-export const db = getFirestore();
-
-export const createUserDocumentFromAuth = async (userAuth) => {
-    const userDocRef = doc(db, 'users', userAuth.uid);
-
-    const userSnapshot = await getDoc(userDocRef);
-
-    //if user data does not exist
-    //create / set the document with the data from userAuth in my collection
-    if (!userSnapshot.exists()) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
-
-        try {
-            await setDoc(userDocRef, {
-                displayName,
-                email,
-                createdAt
-            })
-        } catch (error) {
-            console.log('error creating the user', error.message);
-        }
-    }
-
-    //if user data exists
-    return userDocRef;
-}
